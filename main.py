d3a6034b1f97e62b18d68b726ad01a5777c58edd
IrakliGuliashvili007 <128288227+IrakliGuliashvili007@users.noreply.github.com>
2023-05-09 06:50:24 +0000

    შექმნის VPC ს. დაარქმევს მას სახელს(tag-ის გამოყენებით). შექმნის IGW, IGW ს მიაბამს VPC ს.

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import argparse
 import boto3
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Create a VPC and associate an Internet Gateway with it')
-parser.add_argument('name', metavar='NAME', type=str, help='name for the VPC')
+ec2 = boto3.resource('ec2')
+
+# Define command-line arguments
+parser = argparse.ArgumentParser(description='Create a VPC and an Internet Gateway')
+parser.add_argument('--name', metavar='NAME', type=str, required=True, help='name of the VPC')
+parser.add_argument('--cidr', metavar='CIDR', type=str, default='10.0.0.0/16', help='CIDR block for the VPC')
+parser.add_argument('--region', metavar='REGION', type=str, default='us-east-1', help='AWS region to create the VPC in')
+
+# Parse command-line arguments
 args = parser.parse_args()
 
-# Create a VPC with the given name
-ec2 = boto3.resource('ec2')
-vpc = ec2.create_vpc(CidrBlock='10.0.0.0/16')
-vpc.create_tags(Tags=[{'Key': 'Name', 'Value': args.name}])
-vpc.wait_until_available()
+# Create VPC
+vpc = ec2.create_vpc(CidrBlock=args.cidr)
 
-# Create an Internet Gateway and attach it to the VPC
+# Add tags to VPC
+vpc.create_tags(Tags=[{"Key": "Name", "Value": args.name}])
+
+# Create Internet Gateway
 igw = ec2.create_internet_gateway()
+
+# Attach Internet Gateway to VPC
 igw.attach_to_vpc(VpcId=vpc.id)
 
-print(f"VPC '{args.name}' ({vpc.id}) created with Internet Gateway '{igw.id}'")
+# Add name tag to Internet Gateway
+igw.create_tags(Tags=[{"Key": "Name", "Value": args.name + "-igw"}])
+
+# Print results
+print("VPC created with ID:", vpc.id)
+print("Internet Gateway created with ID:", igw.id)
